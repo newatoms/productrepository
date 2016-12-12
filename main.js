2991bce3bead36fcf74d43294a41f4ed687f169d
Boris van Hoytema <boris@vanhoytema.com>
2016-12-12 11:39:17 +0000

    Move logging up

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ filesToObject(settings.filesLocation, function (err, results) {
   console.log("to the firebase location " + settings.databaseLocation)
   results._updatedAt = new Date().getTime()
   results._updatedBy = 'productrepository'
-  db.ref(settings.databaseLocation).set(results, function () {
-    console.log('Set value to firebase ' + settings.databaseLocation)
-    if(process.env.CIRCLE_ARTIFACTS) {
-      fs.writeFile('results.json', results, function (err) {
-        if (err)
-          return console.error('Error writing output!')
-        console.log('Written output to results.json in the Circle Artifacts folder')
-      })
-    } else {
-      console.log('No artifacts stored')
-    }
+  console.log('Set value to firebase ' + settings.databaseLocation)
+  if(process.env.CIRCLE_ARTIFACTS) {
+    fs.writeFile('results.json', results, function (err) {
+      if (err)
+        return console.error('Error writing output!')
+      console.log('Written output to results.json in the Circle Artifacts folder')
+    })
+  } else {
+    console.log('No artifacts stored')
+  }
+  db.ref(settings.databaseLocation).set(results, function () {  
     console.log('All done!')
     process.exit()
   })
